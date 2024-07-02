17fcc8e467e2c62cf7068d62d596b18e3ea8adb0
Yihen <liuyihen@gmail.com>
2024-07-02 18:00:24 +0800

    add google analytics code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import { RootLayout } from '@/components/RootLayout'
 
 import '@/styles/tailwind.css'
 
+import Script from 'next/script'
+
 export const metadata: Metadata = {
   title: {
     template: '%s - Studio',
@@ -16,6 +18,17 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="h-full bg-neutral-950 text-base antialiased">
       <body className="flex min-h-full flex-col">
+            <Script
+                src="https://www.googletagmanager.com/gtag/js?id=G-HDJNKKEGWD"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-HDJNKKEGWD');
+                `}
+            </Script>
         <RootLayout>{children}</RootLayout>
       </body>
     </html>
